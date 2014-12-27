8bb63ada2ab1f1c5f9bdf2c23d7bc650d3863887
valekar <srinivas.valekar@gmail.com>
2014-12-27 11:54:11 +0530

    menu design

--- client/config/router.js
+++ client/config/router.js
@@ -2,6 +2,10 @@
 Router.route('/',function(){
 
     this.render('mainTemplate');
+
+    this.render('headerTemplate',{to:'header'});
+    this.render('footerTemplate',{to:'footer'});
+
     this.layout('ApplicationLayout');
 
 });
