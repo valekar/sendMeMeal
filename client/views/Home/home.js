03c8f34d33c4407c82cbf3bb688b0092249cb203
valekar <srinivas.valekar@gmail.com>
2014-12-28 23:14:06 +0530

    added login/registration at front end

--- client/views/Home/home.js
+++ client/views/Home/home.js
@@ -1,1 +1,35 @@
+Template.headerTemplate.rendered = function(){
+    $('ul.nav li.dropdown').hover(function(){
+        $('#password').show();
+        $('#forgot_password').show();
+        $('.dropdown-menu',this).parent().addClass('open');
+        $('.dropdown-menu',this).fadeIn('fast');
+
+    },function(){
+        $('.dropdown-menu',this).fadeOut('fast');
+        $('.dropdown-menu',this).parent().removeClass('open');
+    });
+
+    $('ul.nav li.dropdown').click(function(){
+        $('#password').show();
+        $('#forgot_password').show();
+        $('.dropdown-menu',this).fadeIn('fast');
+    });
+
+
+
+};
+
+
+
+
+Template.headerTemplate.events({
+    'click #forgot_password':function(event,templ){
+        $('#password').hide();
+        $('#forgot_password').hide();
+
+    }
+})
+
+
 Meteor.subscribe('itemList');
